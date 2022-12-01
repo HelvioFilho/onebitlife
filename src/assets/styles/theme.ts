f2629473c54ef7ceb5419b29e83944bc83f1a5bd
Hélvio Filho <helviosvf@gmail.com>
2022-11-30 22:17:51 -0300

    Fix: configured styled typescript

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,5 +1,11 @@
 export default {
   colors: {
-
+    background: "rgba(21,21,21, 0.98)",
+    white: "#FFFFFF",
+    card: "#151515",
+    mind: "#90B7F3",
+    money: "#85BB65",
+    body: "#FF0044",
+    fun: "#FE7F23",
   }
 }
